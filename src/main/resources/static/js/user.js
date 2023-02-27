f617bc15783c050f3cb27c74b1f9a200961d330b
Evgen12412 <70172427+Evgen12412@users.noreply.github.com>
2023-02-27 12:13:42 +0300

    Update user.js

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -15,7 +15,28 @@ const showUserInfo = (user) => {
         </tr>`
     container.innerHTML = userPageInfo
 }
+
+
+let userNavbar = '';
+const detailsUser = (user) => {
+    const containerNavbar = document.getElementById("navbarUser");
+    userNavbar += `
+         <span class="navbar-brand mb-0 h1" style="color:white; margin-right: 20px">${user.username}</span>
+         <span class="navbar-brand mb-0 h1" style="color:white">${user.roles.map(role => role.name)}</span>
+         <a href="/logout">
+         <button type="button" class="btn btn-primary mr float-right" style="margin-left: 800px">
+         Выйти
+         </button>
+         </a>                                    
+                                    
+    `
+    containerNavbar.innerHTML = userNavbar
+}
+
 fetch("/api/user/")
     .then(response => response.json())
-    .then(data => showUserInfo(data))
+    .then(data => {
+        showUserInfo(data);
+        detailsUser(data)
+    })
     .catch(error => console.log(error))
